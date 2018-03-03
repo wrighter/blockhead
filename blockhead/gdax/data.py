0cc3ee43e3db7e5d08fa3312e42013f436f8a223
Matt Wright <matt@wrighters.net>
2018-03-03 09:38:42 -0600

    convert dates to UTC

--- blockhead/gdax/data.py
+++ blockhead/gdax/data.py
@@ -6,10 +6,15 @@ Github: https://github.com/wrighter
 Description:  Data file handling for historical gdax data
 """
 
+from __future__ import absolute_import
+
 import pathlib
 import time
 import logging
 import requests
+
+from dateutil.tz import tzutc
+from dateutil.parser import parse
 
 import gdax
 import pandas as pd
@@ -23,31 +28,48 @@ def get_bars(pair, start, end, interval,
     start/end times are in local time, will convert them to UTC.
 
     pair -- the gdax currency pair
-    start -- the start datetime
-    end -- the end datetime
+    start -- the start datetime, or a string that will be parsed
+    end -- the end datetime, or a string that will be parsed
     interval -- the bar interval in seconds
     directory -- the cache directory
     client -- the gdax client to use to fetch bars if needed
     """
+
+    if isinstance(start, str):
+        start = parse(start)
+    if isinstance(end, str):
+        end = parse(end)
     # convert start and end to UTC
     start = to_utc(start)
     end = to_utc(end)
 
     # get date range
-    dates = pd.date_range(start, end) 
+    dates = pd.date_range(start, end, tz='UTC')
     # iterate through dates and build up all the bars
-    path = pathlib.Path(directory, pair, interval)
+    path = pathlib.Path(directory, pair, str(interval))
     all_bars = []
+    def date_utc(col):
+        """ UTC date parser for bars """
+        return parse(col, tzinfos=tzutc)
+
     for date in dates:
         barfile = path / date.strftime('%Y-%m-%d')
         if barfile.exists():
-            all_bars.append(pd.read_csv(barfile.open('w'), index_col=1))
+            logging.debug("loading existing bars for %s", date)
+            bars = pd.read_csv(barfile.open('r'),
+                               parse_dates=[0, 6],
+                               index_col=6,
+                               date_parser=date_utc)
+            bars['close_time'] = bars.index
+            all_bars.append(bars)
         else:
             client = client or gdax.PublicClient()
-            all_bars.append(fetch_bars)
-    return pd.concat[all_bars]
+            dstart = max(start, date.replace(hour=0, minute=0, second=0))
+            dend = min(end, (date + pd.Timedelta('1d')).replace(hour=0, minute=0, second=0))
+            all_bars.append(fetch_bars(pair, dstart, dend, interval, client))
+    return pd.concat(all_bars)[start:end]
 
-def fetch_bars(pair, start, end, interval, client=None, batch=200):
+def fetch_bars(pair, start, end, interval, client=None, batch=350):
     """ fetches bars from GDAX's public api
     pair -- the gdax currency pair
     start -- the start datetime
@@ -56,6 +78,12 @@ def fetch_bars(pair, start, end, interval, client=None, batch=200):
     client -- the gdax client to use to fetch bars if needed
     batch -- the batch size to use for fetching
     """
+    # convert start and end to UTC
+    start = to_utc(start)
+    end = to_utc(end)
+
+    full_start = start
+    full_end = end
     client = client or gdax.PublicClient()
     sleep_time = 0.5
     all_bars = list()
@@ -80,7 +108,7 @@ def fetch_bars(pair, start, end, interval, client=None, batch=200):
 
     all_bars.reverse() # since we went backward in blocks
     bars = pd.concat(all_bars)
-    return bars
+    return bars[full_start:full_end]
 
 def fetch_bar_batch(pair, start, end, interval, client=None):
     """ fetches one batch of bars from GDAX's public api
@@ -90,7 +118,6 @@ def fetch_bar_batch(pair, start, end, interval, client=None):
     interval -- the bar interval in seconds
     client -- the gdax client to use to fetch bars if needed
     """
- 
     client = client or gdax.PublicClient()
     res = client.get_product_historic_rates(pair, start, end, granularity=interval)
     bars = pd.DataFrame(res, columns=['open_time', 'low', 'high', 'open', 'close', 'volume'])
@@ -99,6 +126,7 @@ def fetch_bar_batch(pair, start, end, interval, client=None):
     first_close = bars.loc[bars.index[0], 'open_time'] + pd.Timedelta('%s' % interval)
     bars.loc[bars.index[0], 'close_time'] = first_close
     bars.index = bars['close_time']
+    bars = bars.tz_localize('UTC')
     bars = bars.sort_index()
 
     return bars
